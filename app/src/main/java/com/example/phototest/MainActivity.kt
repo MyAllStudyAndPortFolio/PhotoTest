4ad39f5eec8f2b38332c2f666be392328fc9b60e
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-02-17 14:37:22 +0900

    temp

--- app/src/main/java/com/example/phototest/MainActivity.kt
+++ app/src/main/java/com/example/phototest/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.phototest
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -46,7 +52,8 @@ class MainActivity : AppCompatActivity() {
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED ->{
-                    //TODO 권한이 잘 부여되었을때 갤러리에서 사진을 선택하는 기능
+                    //TODO 권한이 잘 부여되었을때 갤러리에서 사진을 선택하는
+                    navigatePhotos()
                 }
                 //activitycompat를 붙여야 오류가 안난다.
                 ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
@@ -58,6 +65,70 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+
+            1000 -> {
+                // 요청 코드 승인 됫을시
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //todo 권한 부여 됫을시 쓴다.
+                    navigatePhotos()
+                }
+                else{
+                    Toast.makeText(this,"권한 요청을 거부하여 되지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                // 굳이?
+            }
+        }
+
+    }
+
+
+
+    private val resultLauncher: ActivityResultLauncher<Intent>? = null
+
+
+
+
+    private val filterActivityLauncher: ActivityResultLauncher<Intent> =
+
+     registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+
+        }
+
+    private fun navigatePhotos(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        // val resultLauncher: ActivityResultLauncher<Intent>? = null
+        // resultLauncher = registerForActivityResult(
+        //    ActivityResultContracts.StartActivityForResult(),
+        //
+        // )
+
+        //startActivityForResult 가 deprecated 가 되어 다른 걸 써야 한다.
+        filterActivityLauncher.launch(intent)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?)
+    {
+        super.onActivityResult(requestCode, resultCode, data)
+
+
     }
 
     private fun showContextPermissionPopup() {
