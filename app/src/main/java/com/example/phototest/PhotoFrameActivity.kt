bca5b1180e4646fa7456a7069f6d54ba5339ac12
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-02-23 15:02:42 +0900

    finish frame

--- app/src/main/java/com/example/phototest/PhotoFrameActivity.kt
+++ app/src/main/java/com/example/phototest/PhotoFrameActivity.kt
@@ -3,21 +3,86 @@ package com.example.phototest
 import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timer
 
 class PhotoFrameActivity: AppCompatActivity() {
 
     val photoList = mutableListOf<Uri>()
 
+    private var currentPosition =0
+
+    private var timer: Timer? = null
+
+    private val photoImageView : ImageView by lazy {
+        findViewById<ImageView>(R.id.photoImageView)
+    }
+    private val backgroundPhotoImageView: ImageView by lazy {
+        findViewById<ImageView>(R.id.backgroundPhotoImageView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_photoframe)
 
+        getPhotoUriFromIntent()
+
+        //startTimer()
+    }
+
+    private fun getPhotoUriFromIntent(){
         val size = intent.getIntExtra("photoListSize", 0)
         for(i in 0..size){
             intent.getStringExtra("photo$i")?.let {
                 photoList.add(Uri.parse(it))
             }
         }
+    }
+
+    private fun startTimer() {
+        timer = timer(period = 5 * 1000){
+            runOnUiThread{
+
+                Log.d("PhotoFrame", "5 seconds past~!")
+
+                val current = currentPosition
+                val next = if (photoList.size <= currentPosition +1 ) 0 else currentPosition +1
+
+                backgroundPhotoImageView.setImageURI(photoList[current])
+
+                photoImageView.alpha = 0f
+                photoImageView.setImageURI(photoList[next])
+                photoImageView.animate()
+                    .alpha(1.0f)
+                    .setDuration(1000)
+                    .start()
+
+                currentPosition = next
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("PhotoFrame", "onStop!!! timer cancel")
+        timer?.cancel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("PhotoFrame", "onStart!!! timer start")
+
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        timer?.cancel()
     }
 }
